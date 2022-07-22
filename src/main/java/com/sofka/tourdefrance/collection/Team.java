5aaa03928cfc96e539eefdd87b2762f0ddc93c00
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-07-22 17:34:49 -0300

    Country and Team indexation

--- src/main/java/com/sofka/tourdefrance/collection/Team.java
+++ src/main/java/com/sofka/tourdefrance/collection/Team.java
@@ -2,6 +2,7 @@ package com.sofka.tourdefrance.collection;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -10,7 +11,11 @@ public class Team {
 
     @Id
     private String id;
+
+    @Indexed(unique = true)
     private String teamName;
+
+    @Indexed(unique = true)
     private String shortName;
     private String countryId;
     private String countryName;
